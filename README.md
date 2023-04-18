d34bb0f1afa9cb35de4fe794650fc911f4d921ad
NCapeta <ncapeta@kent.edu>
2023-04-18 13:34:36 -0400

    Updating for username to id Updating README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # student-management
 Creating a student management database system
-Using SQL & PHP
+Using SQL, PHP, & HTML
+
+Uses XAMPP to run. Place php files in the htdocs folder. SQL files go in mysql/data folder.
--- index.php
+++ index.php
@@ -14,8 +14,8 @@ if (isset($_GET['stusubmit'])){
         <h5>Student Login</h5>
         <form>
             <div class = "mb-3">
-                <label for = "stuuname">Username: </label><br>
-                <input type = "text" id = "stuuname" name = "stuuname"><br>
+                <label for = "stuid">ID: </label><br>
+                <input type = "text" id = "stuid" name = "stuid"><br>
                 <label for = "password">Password: </label><br>
                 <input type = "text" id = "stupassword" name = "stupassword"><br>
             </div>
@@ -30,8 +30,8 @@ if (isset($_GET['stusubmit'])){
         <h5>Administrator Login</h5>
         <form>
             <div class = "mb-3">
-                <label for = "adminuname">Username: </label><br>
-                <input type = "text" id = "adminuname" name = "adminuname"><br>
+                <label for = "adminid">ID: </label><br>
+                <input type = "text" id = "adminid" name = "adminid"><br>
                 <label for = "password">Password: </label><br>
                 <input type = "text" id = "adminpassword" name = "adminpassword"><br>
             </div>
