ed9b5b184b820516b4a2dc5067dc1244d18d8e8a
NCapeta <ncapeta@kent.edu>
2023-04-18 13:04:57 -0400

    Splitting to have a separate sign on for users and for admins

--- index.php
+++ index.php
@@ -3,18 +3,38 @@
 
 <h2 class = "text-warning"> Hogwarts Student Management </h2>
 
-<form>
-    <div class = "mb-3">
-        <label for = "uname">Username: </label><br>
-        <input type = "text" id = "uname" name = "uname"><br>
-        <label for = "password">Password: </label><br>
-        <input type = "text" id = "password" name = "password"><br>
+<div class = "row">
+    <div class = "col">
+        <form>
+            <div class = "mb-3">
+                <label for = "stuuname">Username: </label><br>
+                <input type = "text" id = "stuuname" name = "stuuname"><br>
+                <label for = "password">Password: </label><br>
+                <input type = "text" id = "stupassword" name = "stupassword"><br>
+            </div>
+        <div class = "mb-3">
+        <div class="col-xs-1" align="center">
+            <input type = "submit" name = "submit" value = "Submit">
+        </div>
+        </div>
+        </form>
     </div>
-     <div class = "mb-3">
-    <div class="col-xs-1" align="center">
-        <input type = "submit" name = "submit" value = "Submit">
+    <div class = "col">
+        <form>
+            <div class = "mb-3">
+                <label for = "adminuname">Username: </label><br>
+                <input type = "text" id = "adminuname" name = "adminuname"><br>
+                <label for = "password">Password: </label><br>
+                <input type = "text" id = "adminpassword" name = "adminpassword"><br>
+            </div>
+        <div class = "mb-3">
+        <div class="col-xs-1" align="center">
+            <input type = "submit" name = "submit" value = "Submit">
+        </div>
+        </div>
+        </form>
+
     </div>
-    </div>
-</form>
+</div>
 
 <?php include 'inc/footer.php'; ?>
