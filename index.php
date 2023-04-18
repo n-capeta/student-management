b63c71d1b2a1a9008e86b346964fb6d0b3337aab
ncapeta <ncapeta@kent.edu>
2023-04-17 23:05:20 -0400

    Adding uname input and color to headers

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php include 'inc/header.php'; ?>
 
 
+<h2 class = "text-warning"> Hogwarts Student Management </h2>
 
-<h2> Hogwarts Student Management </h2>
-
-
+<form>
+    <div class = "mb-3">
+        <label for = "uname">Username: </label><br>
+        <input type = "text" id = "uname" name = "uname"><br>
+        <label for = "password">Password: </label><br>
+        <input type = "text" id = "password" name = "password"><br>
+    </div>
+     <div class = "mb-3">
+    <div class="col-xs-1" align="center">
+        <input type = "submit" name = "submit" value = "Submit">
+    </div>
+    </div>
+</form>
 
 <?php include 'inc/footer.php'; ?>
